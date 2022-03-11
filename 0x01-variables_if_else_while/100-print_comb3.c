3a16442586f66653d2ce9790ef4e01dac1d63151
Simotwo-lab <faithsimotwo@gmail.com>
2022-03-11 15:39:40 -0800

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,46 +1,37 @@
 #include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
+ *  main - Prints numbers between 00 to 89.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
-	int j;
+	int i, e;
 
 	i = 48;
-	j = 49;
-	while  ((i < 57) && (j < 58))
-	{
-		putchar(i);
-		putchar(j);
-		if ((i == 56) && (j == 57))
-		{
-			putchar('\n');
-			i++;
-			j++;
-		}
-		else 
-		{
-			putchar(44);
-			putchar(32);
-			if (j < 57)
-			{
-				j++;
-			}
-			else
-			{
-				i++;
-				j = 1 + i;
-			}
-		}
-	}
+	e = 48;
 
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
 	return (0);
 }
-
