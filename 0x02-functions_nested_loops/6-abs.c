a9443f377201f035ff26fa57505fe3918768a269
Simotwo-lab <faithsimotwo@gmail.com>
2022-03-16 04:23:18 +0300

    nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * * _abs - computes the absolute value of an integer
+ * * @n: n is an integer
+ * *
+ * * Return: integer
+ */
+
+#include "main.h"
+
+/**
+ * * _abs - computes the absolute value of an integer
+ * * @n: n is an integer
+ * *
+ * * Return: integer
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+6-abs.c[+] [unix] (02:59 01/01/1970)                                    10,1 All
+-- INSERT --
+
