10cc3d21300e0946c0f47b03e877bb2bcc61e42c
Simotwo-lab <faithsimotwo@gmail.com>
2022-03-18 01:03:50 +0300

    degging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+oid
+[3~[3~[3~[3~[3~[A[A[B[3~[3~[3~[3~[3~[3~[3~[C[C[C[C[C[Cvoid positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif
