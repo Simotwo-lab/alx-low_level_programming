5cbd78dcfdf0700f8b28c7c89ce1743baf35923c
Simotwo-lab <faithsimotwo@gmail.com>
2022-04-04 13:35:33 +0300

    Static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,8 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
-int _isupper(int c);
+nt _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
@@ -18,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
